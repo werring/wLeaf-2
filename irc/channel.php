e05a689d3639959e6a94acd1e1038c97873f161e
Werring <thomwerring@gmail.com>
2010-07-09 20:36:11 +0200

    fixed getChan

--- irc/channel.php
+++ irc/channel.php
@@ -1,3 +1,19 @@
 <?php
-
+/*
+ * class Irc_Channel
+ */
+class Irc_Channel  {
+    public function getChan(){
+        $chan = Irc_Socket::$eLine[3];
+        if(preg_match("/^\#/",trim($chan))){
+            return $chan;
+        } else {
+            foreach(Irc_Socket::$eLine as $line){
+                if(preg_match("/^\#/",trim($line))){
+                    return $line;
+                }
+            }
+        }
+    }
+}
 ?>
