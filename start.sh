f25b15527b530a53ec58b0dbfbbc78e2a472af08
Werring <thomwerring@gmail.com>
2010-07-11 18:02:13 +0200

    added ./start.sh getopts, now use start.sh -nl to start the bot with nohup and log it to ~/wleafv2/wLeaf.log

--- start.sh
+++ start.sh
@@ -1,3 +1,36 @@
 echo "starting wLeaf v2"
-php start.php
+noHup=0
+logIt=0
+while getopts ":nl" opt
+do
+  case $opt in
+    n)
+      noHup=1
+      ;;
+    l)
+      logIt=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      echo "Posible options are: [nl]"
+      exit
+      ;;
+  esac
+done
 
+if [ $noHup -ne 1 -a $logIt -ne 1 ]
+then
+    php start.php
+else
+    if [ $noHup -eq 1 -a $logIt -ne 1 ]
+    then
+        nohup php start.php
+    else
+        if [ $logIt -eq 1 -a $noHup -ne 1 ]
+        then
+            php start.php > ~/wleafv2/wLeaf.log &
+        else
+            nohup php start.php > ~/wleafv2/wLeaf.log &
+        fi
+    fi
+fi
