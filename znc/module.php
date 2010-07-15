c978300562200cdaee115154197d3c1e96d75257
Werring <thomwerring@gmail.com>
2010-07-15 14:26:10 +0200

    delete accounts that have bee flagged for deletion by cronjob

--- znc/module.php
+++ znc/module.php
@@ -10,7 +10,6 @@ class Znc_Module  {
      * @access public
     */
     public static $setting;
-    
     /**
      * handles text from znc odules
     */
@@ -41,22 +40,35 @@ class Znc_Module  {
                             }
                             if(self::$setting["+counter"]==2)
                                 Irc_Socket::write("NOTICE " . self::$setting["sender"] . " : - " . self::$setting["params"][0]);
-                            
                             self::$setting["params"] = array();
                         }
-
                     break;
                     case "block":
-                        
                     break;
                 }
             break;
             case "status":
-                
             break;
-            
         }
-    
+    }
+    public function cronjobDeleteAccounts(){
+        $files = scandir("removeUserFromZNC");
+        if(count($files)>2){
+            array_shift($files);
+            array_shift($files);
+            $filepath = "removeUserFromZNC/";
+            foreach($files as $file){
+                    $fileText = file_get_contents($filepath. $file);
+                    $data    = explode("|",$fileText);
+                    $account = trim($data[0]);
+                    $auth    = trim($data[1]);
+                    $access  = trim($data[2]);
+                    unlink($filepath.$file);
+                    Irc_Socket::write("cs #tree fire *" . $auth);
+                    Irc_Socket::write("ZNC *admin deluser " . $account);
+                    Irc_Format::log("Cronjob deleted user " . $account . " (".$auth.") Access: " . $access,"CRON");
+            }
+        }    
     }
 }
 ?>
