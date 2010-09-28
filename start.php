635fc7110a10edbaff41a2a5a27574af3835a290
werring <thomwerring@gmail.com>
2010-09-28 15:56:38 +0200

    fixed indent

--- start.php
+++ start.php
@@ -26,53 +26,53 @@ function __autoload($class_name) {
 }
 
 Irc_Format::log("Starting wLeaf v2.1.0");
-    Irc_Format::log("init database");
-    $sql = new Database_Mysql();
-    $sql->clear("IrcUserData");
-    Irc_Format::log("init configuration");
-    $config = new WleafConfig();
-    Irc_Format::log("init irc connection");
-    $socket = new Irc_Socket();
+Irc_Format::log("init database");
+$sql = new Database_Mysql();
+$sql->clear("IrcUserData");
+Irc_Format::log("init configuration");
+$config = new WleafConfig();
+Irc_Format::log("init irc connection");
+$socket = new Irc_Socket();
+
+/**
+ * IRC startup 
+ *
+ * sends user, nick and password lines
+*/
+$socket->write("USER ".$config->getConf("ident")." ".$config->getConf("ident")." ".$config->getConf("server")." :".$config->getConf("realname"));
+$socket->write("PASS ".$config->getConf("password"));
+$socket->write("NICK ".$config->getConf("nick"));
+
+while(Irc_Socket::$connected){
+    $time = time();
+    /**
+     * DEBUG only run for ~ 120 seconds
+    */
+    if(($time-120)>$config->getConf("startupTime") && DEBUG){
+        $socket->write("QUIT");
+        sleep(1);
+        $socket->close("Closing connection after 2 minutes of testing");
+    }
+    $socket->readline();
     
     /**
-     * IRC startup 
+     * incomming lines
      *
-     * sends user, nick and password lines
+     * Prevends double handling of lines and handling empty lines
     */
-    $socket->write("USER ".$config->getConf("ident")." ".$config->getConf("ident")." ".$config->getConf("server")." :".$config->getConf("realname"));
-    $socket->write("PASS ".$config->getConf("password"));
-    $socket->write("NICK ".$config->getConf("nick"));
-    
-    while(Irc_Socket::$connected){
-        $time = time();
-        /**
-         * DEBUG only run for ~ 120 seconds
-        */
-        if(($time-120)>$config->getConf("startupTime") && DEBUG){
-            $socket->write("QUIT");
-            sleep(1);
-            $socket->close("Closing connection after 2 minutes of testing");
-        }
-        $socket->readline();
-        
-        /**
-         * incomming lines
-         *
-         * Prevends double handling of lines and handling empty lines
-        */
-        $data = Irc_Socket::$line . $time;
-        if($time === $data || strlen($time)===strlen($data) || $lastLine === $data){
-            continue;
-        } 
-        $lastLine = $data;
-        /**
-         * Event handler
-        */
-        Irc_Handle::handle();
-        Znc_Module::cronjobDeleteAccounts();
-    }
-    if(!DEBUG && Irc_Socket::$reconnection){
-        Irc_Format::log("Atempt to restart wLeaf");
-    }
+    $data = Irc_Socket::$line . $time;
+    if($time === $data || strlen($time)===strlen($data) || $lastLine === $data){
+        continue;
+    } 
+    $lastLine = $data;
+    /**
+     * Event handler
+    */
+    Irc_Handle::handle();
+    Znc_Module::cronjobDeleteAccounts();
+}
+if(!DEBUG && Irc_Socket::$reconnection){
+    Irc_Format::log("Atempt to restart wLeaf");
+}
 
 ?>
