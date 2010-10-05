b62199ff00293edd16fab24c85398b412f701921
werring <thomwerring@gmail.com>
2010-10-05 21:33:16 +0200

    multiple return lines are now supported in this command

--- irc/commands/general/php.php
+++ irc/commands/general/php.php
@@ -1,6 +1,19 @@
 <?php
-if(count(Irc_Command::$params)>0)
-    Irc_Socket::noticeNick(eval("return " . implode(" ",Irc_Command::$params) . ";"));
-else
+if(count(Irc_Command::$params)>0){
+    $output = eval("return " . implode(" ",Irc_Command::$params) . ";");
+    $output = explode("\n",$output);
+    foreach($output as $lineNumber => $line){
+        switch($lineNumber){
+            case ($lineNumber > 15):
+                sleep(1);
+            case ($lineNumber > 10):
+                sleep(1);
+            case ($lineNumber > 5):
+                sleep(1);
+        }
+        Irc_Socket::noticeNick($line);
+    }
+} else {
     Irc_Socket::noticeNick("No text to send");
+}
 ?>
