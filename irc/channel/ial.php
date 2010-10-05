da2d59bdb051bb96745009021c2a0fa46953f3f1
werring <thomwerring@gmail.com>
2010-10-05 17:06:43 +0200

    wrote rest of the functions, also added the network var in the mysql queries

--- irc/channel/ial.php
+++ irc/channel/ial.php
@@ -18,6 +18,11 @@
             self::$networkDefined = true;
         }
         
+        /**
+         * addNick
+         * adds a nick to the IAL
+         * 
+        */
         protected function addNick($channel,$nick, $ident, $host){
             if(self::$networkDefined){
                 $data['nick']    = $nick;
@@ -25,7 +30,15 @@
                 $data['host']    = $host;
                 $data['channel'] = $channel;
                 $data['network'] = self::$network;
-                Database_Mysql::insert('InternalAdressList',$data);
+                $returnData = Database_Mysql::select('InternalAdressList',"COUNT(*)",$data);
+                if($returnData['affectedRows'] == 0){
+                    $data['nick']    = $nick;
+                    $data['ident']   = $ident;
+                    $data['host']    = $host;
+                    $data['channel'] = $channel;
+                    $data['network'] = self::$network;
+                    Database_Mysql::insert('InternalAdressList',$data);
+                }
                 return true;
             } else {
                 return false;
@@ -33,8 +46,9 @@
         }
         protected function changeNick($oldNick,$newNick){
             if(self::$networkDefined){
-                $data['nick']=$newNick;
-                $where['nick'] = $oldNick;
+                $data['nick']     = $newNick;
+                $where['nick']    = $oldNick;
+                $where['network'] = self::$network;
                 $return = Database_Mysql::update('InternalAdressList',$data,$where,0);
                 if($return['errorno'] === 0){
                     return false;
@@ -47,28 +61,55 @@
         }
         protected function quitNick($nick){
             if(self::$networkDefined){
-                
+                $where['nick']    = $nick;
+                $where['network'] = self::$network;
+                Database_Mysql::remove('InternalAdressList',$where,0);
+                return true;
             } else {
                 return false;
             }
         }
         protected function removeNick($channel,$nick){
             if(self::$networkDefined){
-                
+                $where['nick']    = $nick;
+                $where['channel'] = $channel;
+                $where['network'] = self::$network;
+                Database_Mysql::remove('InternalAdressList',$where,1);
+                return true;
             } else {
                 return false;
             }        
         }
-        public function channelList($channel){
+        public function userList($channel){
             if(self::$networkDefined){
-                
+                $select[]         = "nick";
+                $select[]         = "ident";
+                $select[]         = "host";
+                $where['channel'] = $channel;
+                $where['network'] = self::$network;
+                $order['nick']    = "ASC";
+                $data             = Database_Mysql::select('InternalAdressList',$fields,$where,0,0,$order);
+                foreach($data as $key => $row){
+                    if(is_numeric($key)){
+                        $return[] = $row['nick'] . "!" . $row['ident'] . "@" . $row['host'];
+                    }
+                }
             } else {
                 return false;
             }
         }
         public function getFullHost($nick){
             if(self::$networkDefined){
-                
+                $select[]         = "ident";
+                $select[]         = "host";
+                $where['nick']    = $nick;
+                $where['network'] = self::$network;
+                $data             = Database_Mysql::select('InternalAdressList',$select,$where,1);
+                if($data['affectedRows'] == 1){
+                    return $nick . "!" . $data[0]['ident'] . "@" . $data[0]['host'];
+                } else {
+                    return false;
+                }
             } else {
                 return false;
             }
@@ -95,24 +136,34 @@
                         }
                         $return = self::addNick($channel,$nick,$ident,$host);
                     break;
-                    case 'PART':
-                    break;
-                    case 'KICK':
-                    break;
-                    case 'QUIT':
-                    break;
-                    case 'NICK':
-                        $eData   = explode("!",Irc_Socket::$eLine[0]);
-                        $oldNick = array_shift($eData);
-                        $newNick = Irc_Format::removeColon(Irc_Socket::$eLine[2]);
-                        $return = self::changeNick($oldNick,$newNick);
-                    break;
                     case '352':
                         $channel = Irc_Socket::$eLine[3];
                         $nick    = Irc_Socket::$eLine[7];
                         $ident   = Irc_Socket::$eLine[4];
                         $host    = Irc_Socket::$eLine[5];
                         $return  = self::addNick($channel,$nick,$ident,$host);
+                    break;
+                    case 'PART':
+                        $eData   = explode("!",Irc_Format::removeColon(Irc_Socket::$eLine[0]));
+                        $nick    = array_shift($eData);
+                        $channel = Irc_Socket::$eLine[2];
+                        $return  = self::removeNick($channel,$nick);
+                    break;
+                    case 'KICK':
+                        $channel = Irc_Socket::$eLine[2];
+                        $nick    = Irc_Socket::$eLine[3];
+                        $return  = self::removeNick($channel,$nick);
+                    break;
+                    case 'QUIT':
+                        $eData   = explode("!",Irc_Format::removeColon(Irc_Socket::$eLine[0]));
+                        $nick    = array_shift($eData);
+                        $return  = self::quitNick($nick);
+                    break;
+                    case 'NICK':
+                        $eData   = explode("!",Irc_Socket::$eLine[0]);
+                        $oldNick = array_shift($eData);
+                        $newNick = Irc_Format::removeColon(Irc_Socket::$eLine[2]);
+                        $return  = self::changeNick($oldNick,$newNick);
                     break;
                 }
                 if($return === false){
