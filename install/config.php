6723259f63f18050f55d1d49b90240a367c206fd
werring <thomwerring@gmail.com>
2010-10-05 17:20:00 +0200

    Added network name

--- install/config.php
+++ install/config.php
@@ -1,17 +1,18 @@
 <?php
- $settings['nick']     = 'wLeaf';                 //nickname
- $settings['ident']    = 'wLeaf';                 //ident
- $settings['bindHost'] = '0';                     //bindhost
- $settings['realname'] = 'TreeZNC admin bot';     //realname 
- $settings['server']   = 'user.znc.treefamily.nl';//server
- $settings['port']     = '6667';                  //serverport
- $settings['password'] = 'wLeaf:tree';            //serverpass
- $settings['cmdchar']  = ',';                     //command prefix
- $settings['mainChan'] = '#tree';                 //main channel
- $settings['teamChan'] = '#tree-admin';           //team channel
+ $settings['nick']     = 'wLeaf';                   //nickname
+ $settings['ident']    = 'wLeaf';                   //ident
+ $settings['bindHost'] = '0';                       //bindhost
+ $settings['realname'] = 'TreeZNC admin bot';       //realname 
+ $settings['server']   = 'user.znc.treefamily.nl';  //server
+ $settings['port']     = '6667';                    //serverport
+ $settings['password'] = 'wLeaf:tree';              //serverpass
+ $settings['cmdchar']  = ',';                       //command prefix
+ $settings['mainChan'] = '#tree';                   //main channel
+ $settings['teamChan'] = '#tree-admin';             //team channel
+ $settings['network']  = 'OGN';                     //network name (for IAL)
 
- $master['auth']       = 'Werring';               //authname of master
- $master['account']    = 'Werring';               //ZNC accountname of master
+ $master['auth']       = 'Werring';                 //authname of master
+ $master['account']    = 'Werring';                 //ZNC accountname of master
  
- $znc['znc.conf'] = 'znc.conf';                   //ZNC configfile location
+ $znc['znc.conf'] = 'znc.conf';                     //ZNC configfile location
 ?>
